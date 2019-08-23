d7e6750ec87a669a36c21c3a23b97e86b8ee17ff
MotassemJalal <motassem.jalal-aldeen@t-systems.com>
2019-08-23 10:29:59 +0200

    Added basic material theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
